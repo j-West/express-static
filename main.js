809b34ef803f5588a70848a9d73689a3f29fc27c
j-west <westja89@gmail.com>
2017-03-22 14:14:18 -0500

    Add app.use and express.static to point express to the public directory with static files to be served

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-#!/usr/bin/env node
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello World</h1>`)
+})
+
+
+
+
+app.listen(8080);
